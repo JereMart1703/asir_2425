327e60e7286c1e9aa83ccd4e4b12bd39e0ae116a
JereMart1703 <jemartrog1703@gmail.com>
2025-05-30 18:40:42 +0200

    a

--- fastapi/main.py
+++ fastapi/main.py
@@ -7,12 +7,13 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return ("hello world")
+    return ("aaaaaaa")
     
-
-@app.get("/items/{items_id}")
-def read_items(items_id: int, q: Union[str, None] = None):
-    return {"items_id": items_id, "q": q}   
+@app.get("/hola")
+def hola():
+    return ("mundillo")
+    
+   
 
 
 

4587b928a543d5e0cb46c07015c292f5d38bb06c
JereMart1703 <jemartrog1703@gmail.com>
2025-05-30 19:33:23 +0200

    practicametne

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,19 +1,28 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
-
-
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 def read_root():
     return ("aaaaaaa")
     
 @app.get("/hola")
-def hola():
-    return ("mundillo")
+def hola(request : Request):
+    return templates.TemplateResponse(request=request ,name="hola.html", context={"nombre": "jeremy"})
     
    
-
+@app.get("/ejercicio2")
+def pagina(request : Request):
+    apellido : str = "martinez"
+    edad : int = 20
+    return templates.TemplateResponse(request=request ,name="pagina.html", context={"apellidos": apellido , "edad": edad})
+    
+   
 
 

784ad88a2917c0e241aa2b2a6f893bce568b9159
JereMart1703 <jemartrog1703@gmail.com>
2025-05-31 10:47:11 +0200

    practicametne1

--- fastapi/main.py
+++ fastapi/main.py
@@ -2,8 +2,7 @@ from typing import Union
 
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles 
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
